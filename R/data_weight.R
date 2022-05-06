8a208cfa0cde026a060cbd2c6e7e9228d7b2811e
berkosarFIND <berra.erkosar@finddx.org>
2022-05-06 09:54:25 +0000

    This commit introduces the bmi package and its documentation

---
+++ R/data_weight.R
@@ -0,0 +1,11 @@
+#' Weights and Heights #'
+#' A dataset containing the height and weight data
+#' @format A data frame with 87 rows and 3 columns: #' \describe{
+#' \item{name}{name, a name}
+#' \item{height}{height in meters}
+#' \item{mass}{mass in kg}
+#' #' ...
+#' #' }
+#'  @source \url{www.thinkr.com}
+"data_weight"
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(berraerkosar)
+
+test_check("berraerkosar")
